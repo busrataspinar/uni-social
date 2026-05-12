3758670871442262e4811dbed80527847c369100
yagmur nisa suslu <yagmurnisasuslu@gmail.com>
2026-05-13 01:22:26 +0300

    Cleanup: removed accidental file

---
+++ src/controllers/GonderiYonetici.py
@@ -0,0 +1,23 @@
+class GonderiYonetici:
+    def __init__(self):
+        # Veritabanı bağlantısını başlatır
+        pass
+
+    def gonderi_olustur(self, yazar_id, icerik):
+        # 1. İçerik kontrolü yap
+        # 2. Yeni ID ve tarih oluştur
+        # 3. Modeli kullanarak nesne yarat
+        # 4. Veritabanına kaydet
+        pass
+
+    def gonderi_sil(self, gonderi_id, kullanici_id):
+        # 1. Gönderiyi ID ile bul
+        # 2. Silmek isteyen kişi yazar mı kontrol et
+        # 3. Yetki varsa veritabanından sil
+        pass
+
+    def gonderi_duzenle(self, gonderi_id, kullanici_id, yeni_icerik):
+        # 1. Gönderiyi ID ile bul
+        # 2. Düzenlemek isteyen kişi yazar mı kontrol et
+        # 3. Yetki varsa içeriği güncelle ve kaydet
+        pass
